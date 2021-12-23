1259a0a3566004d0f35bf0af4303e046d1aec81b
yijie4188 <yijie4188@gmail.com>
2021-12-23 15:47:15 +0800

    refactor: optimize code

--- src/initRepoCard.ts
+++ src/initRepoCard.ts
@@ -8,6 +8,16 @@ export interface RepoCardComp {
 export let curDraggedIndex = -1
 export const repoCardRefs = <RepoCardComp[]>[]
 
+const formObserver = new MutationObserver(async () => {
+  const { form } = repoCardRefs[curDraggedIndex]
+  if (form)
+    await fetch(form.action, {
+      method:'post',
+      headers: { accept: 'application/json' },
+      body: new FormData(form)
+    })
+})
+
 export function initRepoCards() {
   document.querySelectorAll<HTMLDivElement>('div.col-12.d-block.width-full.py-4.border-bottom.color-border-muted')
     .forEach(async (div, index) => {
@@ -19,21 +29,27 @@ export function initRepoCards() {
       const form = await getRepoListForm(div)
       if (form) {
         ref.form = form
+        form.querySelectorAll<HTMLInputElement>('input[type=checkbox].js-user-list-menu-item')
+          .forEach(input => {
+            input.addEventListener('change', _ => {
+              curDraggedIndex = index
+              input.toggleAttribute('checked', input.checked)
+            })
+            formObserver.observe(input, {
+              attributes: true,
+              attributeFilter: ['checked']
+            })
+          })
         div.appendChild(form)
       }
       repoCardRefs[index] = ref
     })
 
-  document.querySelectorAll('div.Box > a.d-block.Box-row.Box-row--hover-gray.mt-0.color-fg-default.no-underline')
-    .forEach(ele => {
-      const a = <HTMLAnchorElement>ele
-      a.addEventListener('dragover', event => {
-        event.preventDefault()
-      })
-      a.addEventListener('dragenter', event => {
-        event.preventDefault()
-      })
-      a.addEventListener('drop', async _ => {
+  document.querySelectorAll<HTMLAnchorElement>('div.Box > a.d-block.Box-row.Box-row--hover-gray.mt-0.color-fg-default.no-underline')
+    .forEach(a => {
+      a.addEventListener('dragover', event => event.preventDefault())
+      a.addEventListener('dragenter', event => event.preventDefault())
+      a.addEventListener('drop', async () => {
         if (curDraggedIndex === -1)
           return
 
@@ -41,21 +57,13 @@ export function initRepoCards() {
         if (!form)
           return
 
-        const targetList = a.querySelector('h3.f4.text-bold') as HTMLHeadingElement
+        const targetList = a.querySelector<HTMLHeadingElement>('h3.f4.text-bold')
         form.querySelectorAll('div.form-checkbox').forEach(div => {
-          const input = div.querySelector('input.js-user-list-menu-item') as HTMLInputElement
-          const label = div.querySelector('span.Truncate-text') as HTMLSpanElement
-          if (label.innerText === targetList.innerText) {
-            input.checked = true
+          const input = div.querySelector<HTMLInputElement>('input.js-user-list-menu-item')
+          const label = div.querySelector<HTMLSpanElement>('span.Truncate-text')
+          if (label && input && (label.innerText === targetList?.innerText ?? '')) {
+            input.toggleAttribute('checked', true)
           }
-          // console.log(label.innerText, input.checked)
-        })
-        await fetch(form.action, {
-          method:'post',
-          headers: {
-            accept: 'application/json'
-          },
-          body: new FormData(form)
         })
       })
     })

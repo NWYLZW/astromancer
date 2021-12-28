8a7e730df46dcd04721bd1ebfca3c267c4412178
Sameliya <chenbolang@tal.com>
2021-12-28 16:08:39 +0800

    feat: watch hook

--- src/watchGithub.ts
+++ src/watchGithub.ts
@@ -1,1 +1,21 @@
 console.log('watch Github')
+const script = document.createElement('script')
+script.innerHTML = `const _historyWrap = function (type) {
+  const orig = history[type];
+  const e = new Event(type);
+  return function () {
+    const rv = orig.apply(this, arguments);
+    e.arguments = arguments;
+    window.dispatchEvent(e);
+    return rv;
+  };
+};
+history.pushState = _historyWrap("pushState");
+history.replaceState = _historyWrap("replaceState");
+window.addEventListener("replaceState", function (e) {
+  const regex = /^https?:\\/\\/github\\.com\\/.*\\?tab=star/;
+  if(regex.test(e.arguments[e.arguments.length - 1])) {
+    window.location.reload();
+  }
+});`
+document.head.appendChild(script)
